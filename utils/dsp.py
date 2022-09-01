67f9fb1c763c0eaadaed0238f8aba6c356b2c3ef
xfding <xfding@vw-mobvoi.com>
2022-09-01 16:23:38 +0800

    nr fix

--- utils/dsp.py
+++ utils/dsp.py
@@ -5,39 +5,46 @@ import ctypes
 import numpy as np
 import librosa
 import soundfile as sf
+import sys
+import gc
+gc.disable()
+
+sys.settrace
 
 class DSP():
 
-    def init(self):
-        #so = cdll.LoadLibrary("./libvwmdsp.so")
-        self.so = ctypes.CDLL("./libvwmdsp.so")
+    def init(self, sample_rate, nr_type, level):
+        self.so = ctypes.CDLL("./libnr.so")
 
-        self.so.vwm_uplink_init.restype=ctypes.c_void_p
-        self.so.vwm_uplink_process_ctl.argtypes=[ctypes.c_void_p, ctypes.c_int, ctypes.c_void_p]
-        self.so.vwm_uplink_process.argtypes=[ctypes.c_void_p, ctypes.POINTER(ctypes.c_short), ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_short), ctypes.c_int]
-        self.instance = self.so.vwm_uplink_init(10, 16000, 1, 16000, 1, 1, 1)
-        self.so.vwm_uplink_process_ctl(self.instance, 0, "./")
+        self.so.nr_init.argtypes=[ctypes.c_int, ctypes.c_int, ctypes.c_int]
+        self.so.nr_process.restype=ctypes.POINTER(ctypes.c_short)
+        self.so.nr_process.argtypes=[ctypes.POINTER(ctypes.c_short)]
+        self.so.nr_init(16000, 2, 4)
+    
+    def release(self):
+        self.so.nr_release()
 
-    def denoise(self, wav, samplerate):
+    def denoise(self, data):
+        arr_c = (ctypes.c_short * len(data))(*data)
+        tmp = self.so.nr_process(arr_c)
+        return np.ctypeslib.as_array(tmp, shape=(len(data),)).tolist()
+
+    def denoise_wav(self, wav):
         frame_size = 160;
         output=[wav[i:i + frame_size] for i in range(0, len(wav), frame_size)]
         d = []
         for x in output:
             x = (x * 32767).astype(np.int16)
-            arr_c = (ctypes.c_short * len(x))(*x)
-            
-            tmp =self.so.vwm_uplink_process(self.instance, arr_c, len(x), 1, 0, arr_c, 1)
-            d += np.ctypeslib.as_array(arr_c).tolist()
-        #print(len(output[0]), len(output))
+            d += self.denoise(x)
         d = np.array(d)
         d = (d / 32767).astype(np.float32)
         return d;
 
 if __name__ == '__main__':
     dsp = DSP();
-    dsp.init()
-    data, fs = sf.read('/data/xfding/xfding_en.wav')
-    print(len(data), fs)
-    d = dsp.denoise(data, fs)
-    sf.write("../denoised.wav", d, fs)
+    data, fs = sf.read('./samples/xfding_en.wav')
+    dsp.init(fs, 1, 4)
+    d = dsp.denoise_wav(data)
+    dsp.release()
+    sf.write("./denoised.wav", d, fs)
 
